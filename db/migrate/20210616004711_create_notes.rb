102adfa2ae9bc63398d25f427bb071a2140e8b5b
Mac Rowe <m.rowe42@gmail.com>
2021-06-15 20:50:00 -0400

    Generate scaffold for film notes

---
+++ db/migrate/20210616004711_create_notes.rb
@@ -0,0 +1,12 @@
+class CreateNotes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :notes do |t|
+      t.string :title
+      t.string :content
+      t.string :time_marker
+      t.belongs_to :film, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
