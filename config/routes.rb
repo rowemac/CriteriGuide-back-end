1afbf47d4a0fcb5b7d9113d2adacfd2ea1ab268e
Mac Rowe <m.rowe42@gmail.com>
2021-06-18 10:09:00 -0400

    Add custom route for Note POST action

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   
   resources :films do
     resources :notes
-  end 
+  end
+
+  post '/films/:id/notes', to: 'notes#index', as: 'note'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
