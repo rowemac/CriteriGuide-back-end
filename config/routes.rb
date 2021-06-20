147468861eed2426d52bd205eb0f3dc34e356034
Mac Rowe <m.rowe42@gmail.com>
2021-06-20 10:42:09 -0400

    Update routes to have resources for note

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   
+
   resources :films do
     resources :notes
   end
 
-  post '/films/:id/notes', to: 'notes#index', as: 'note'
+  resources :notes
+
+
+  # post '/films/:id/notes', to: 'notes#index', as: 'note'
+
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
